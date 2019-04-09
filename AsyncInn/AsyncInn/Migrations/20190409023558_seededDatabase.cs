3c4efd25b1f58b39aced1d21d48012ad45a57fef
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-08 19:47:12 -0700

    Seed DB and create scafolded Views and Controllers.

---
+++ AsyncInn/AsyncInn/Migrations/20190409023558_seededDatabase.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class seededDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "A/C" },
+                    { 2, "Mini Fridge" },
+                    { 3, "Spa" },
+                    { 4, "Hot Tub" },
+                    { 5, "Flatscreen TV" },
+                    { 6, "Balcony" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "ID", "City", "Name", "PhoneNumber", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 5, "Los Angeles", "Sheraton", "555-555-5555", "CA", "123 Fake St." },
+                    { 4, "Monroe", "Motel 6", "555-555-5555", "WA", "123 Fake St." },
+                    { 3, "Vancouver", "Red Lion", "555-555-5555", "WA", "123 Fake St." },
+                    { 2, "Portland", "Hilton Inn", "555-555-5555", "OR", "123 Fake St." },
+                    { 1, "Seattle", "Best Western", "555-555-5555", "WA", "123 Fake St." }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "ID", "Layout", "Name" },
+                values: new object[,]
+                {
+                    { 1, 0, "Disney Dorm" },
+                    { 2, 0, "Ocean Shores Bungalo" },
+                    { 3, 1, "Reinier View" },
+                    { 4, 1, "Scenic Skyloft" },
+                    { 5, 2, "Hawk Hights" },
+                    { 6, 2, "Seahawk Snooze" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 6);
+        }
+    }
+}
