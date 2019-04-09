e4cbd7f3962cc6d4801f1a3225e4204d79ef6af9
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-09 13:35:50 -0700

    Creates initial Interface and Service for Hotels.

---
+++ AsyncInn/AsyncInn/Models/Interfaces/IHotelManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    public interface IHotelManager
+    {
+        // Create a Hotel
+        Task CreateHotel(Hotel hotel);
+
+        // Update a Hotel
+        void UpdateHotel(int id);
+
+        // Delete a Hotel
+        bool DeleteHotel(int id);
+
+        // Get a single Hotel's Details
+        Hotel GetHotel(int id);
+
+        // Get all Hotel's Details
+        Task<List<Hotel>> GetHotels();
+    }
+}
---
+++ AsyncInn/AsyncInn/Models/Services/HotelService.cs
@@ -0,0 +1,46 @@
+﻿using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Services
+{
+    public class HotelService : IHotelManager
+    {
+        private AsyncInnDbContext _context;
+
+        public HotelService(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateHotel(Hotel hotel)
+        {
+            _context.Add(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+        public bool DeleteHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hotel GetHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Hotel>> GetHotels()
+        {
+            return await _context.Hotels.ToListAsync();
+        }
+
+        public void UpdateHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
